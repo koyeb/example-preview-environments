5db4556c64b76f78085941cae7d623bcb0b06eca
Justin Ellingwood <justin.ellingwood@gmail.com>
2023-11-02 15:31:56 +0000

    Increase message size

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('<h2>Hello, world!</h2>')
+    res.send('<h1>Hello, world!</h1>')
 })
 
 app.listen(port, () => {
